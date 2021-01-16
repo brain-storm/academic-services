22c769f9d7d9971a4de9efe8441aaf25404da865
SwayambhooM <swayam.ece007@gmail.com>
2021-01-16 22:08:00 +0530

    Added study material in subject model

--- models/subject.js
+++ models/subject.js
@@ -15,9 +15,34 @@ const subjectSchema = new Schema({
         required:false,
         type : mongoose.Schema.Types.ObjectId,
         ref : 'Teacher'
-    },
+    }, 
+    studyMaterial:{
+        type :[{
+            name:{
+                type:  String,
+                required : true
+            },
+            description : String,
+            url : String
+        }],
+        required : false ,
+        _id: false
+    }
 })
 
 const Subject = mongoose.model('Subject' , subjectSchema);
 
 module.exports = Subject;
+
+/*
+To be added
+All of these will reference the subject :-
+
+quiz
+response
+
+assignment
+response
+
+material
+*/ 
